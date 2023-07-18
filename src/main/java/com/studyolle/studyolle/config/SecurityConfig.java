a6e7bac988e4e6969385df13f514c9b17d604a66
noname_dev <noname_dev@naver.com>
2023-07-19 00:41:59 +0900

    계정 도메인, 회원 가입 컨트롤러, 컨트롤러 테스트

---
+++ src/main/java/com/studyolle/studyolle/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.studyolle.studyolle.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/","/login","/sign-up","/check-email","/check-email/token",
+                        "/email-login","/check-email-login","/login-link").permitAll()
+                .mvcMatchers(HttpMethod.GET,"/profile/*").permitAll()
+                .anyRequest().authenticated();
+
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/resources/**");
+    }
+}
