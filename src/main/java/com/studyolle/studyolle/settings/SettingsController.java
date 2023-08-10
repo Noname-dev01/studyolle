c102416dab6bca00a402b536307fb958203dd18a
noname_dev <noname_dev@naver.com>
2023-08-10 18:12:48 +0900

    프로필 이미지 변경

--- src/main/java/com/studyolle/studyolle/settings/SettingsController.java
+++ src/main/java/com/studyolle/studyolle/settings/SettingsController.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -17,6 +19,11 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class SettingsController {
 
+    @InitBinder("passwordForm")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(new PasswordFormValidator());
+    }
+
     //오타 방지로 빼놓음
     static final String SETTINGS_PROFILE_VIEW_NAME = "settings/profile";
     static final String SETTINGS_PROFILE_URL = "/settings/profile";
@@ -24,7 +31,7 @@ public class SettingsController {
     private final AccountService accountService;
 
     @GetMapping(SETTINGS_PROFILE_URL)
-    public String profileUpdateForm(@CurrentUser Account account, Model model){
+    public String updateProfileForm(@CurrentUser Account account, Model model){
 
         model.addAttribute(account);
         model.addAttribute(new Profile(account));
@@ -46,4 +53,24 @@ public class SettingsController {
         return "redirect:" + SETTINGS_PROFILE_URL;
     }
 
+    @GetMapping("/settings/password")
+    public String updatePasswordForm(@CurrentUser Account account, Model model){
+        model.addAttribute(account);
+        model.addAttribute(new PasswordForm());
+
+        return "settings/password";
+    }
+
+    @PostMapping("/settings/password")
+    public String updatePassword(@CurrentUser Account account,@Valid PasswordForm passwordForm, Errors errors,
+                                 Model model, RedirectAttributes attributes){
+        if (errors.hasErrors()){
+            model.addAttribute(account);
+            return "settings/password";
+        }
+
+        accountService.updatePassword(account, passwordForm.getNewPassword());
+        attributes.addFlashAttribute("message", "패스워드를 변경했습니다.");
+        return "redirect:/settings/password";
+    }
 }
