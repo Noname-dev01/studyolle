bbe4b09206e425412759ade3bff2ce9a007a5c63
noname_dev <noname_dev@naver.com>
2023-08-23 17:03:17 +0900

    스터디 구성원 조회

--- src/main/java/com/studyolle/studyolle/study/StudyController.java
+++ src/main/java/com/studyolle/studyolle/study/StudyController.java
@@ -44,8 +44,9 @@ public class StudyController {
     }
 
     @PostMapping("/new-study")
-    public String newStudyForm(@CurrentUser Account account, @Valid StudyForm studyForm, Errors errors){
+    public String newStudyForm(@CurrentUser Account account, @Valid StudyForm studyForm, Errors errors, Model model){
         if (errors.hasErrors()){
+            model.addAttribute(account);
             return "study/form";
         }
 
@@ -59,5 +60,13 @@ public class StudyController {
         model.addAttribute(studyRepository.findByPath(path));
 
         return "study/view";
+    }
+
+    @GetMapping("/study/{path}/members")
+    public String viewStudyMembers(@CurrentUser Account account, @PathVariable String path, Model model){
+        model.addAttribute(account);
+        model.addAttribute(studyRepository.findByPath(path));
+
+        return "study/members";
     }
 }
