64ac4c5b4de85ee8b2e133fdc3cc090dcba93e9a
noname_dev <noname_dev@naver.com>
2023-09-01 17:44:08 +0900

    모임 수정

--- src/main/java/com/studyolle/studyolle/domain/Event.java
+++ src/main/java/com/studyolle/studyolle/domain/Event.java
@@ -87,4 +87,8 @@ public class Event {
         }
         return false;
     }
+
+    public long getNumberOfAcceptedEnrollments() {
+        return this.enrollments.stream().filter(Enrollment::isAccepted).count();
+    }
 }
