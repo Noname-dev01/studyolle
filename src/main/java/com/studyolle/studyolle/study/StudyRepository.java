18701c399fa5dce36c4cb20c0ef68080f439b18e
noname_dev <noname_dev@naver.com>
2023-09-01 19:10:39 +0900

    모임 참가 신청 및 취소

--- src/main/java/com/studyolle/studyolle/study/StudyRepository.java
+++ src/main/java/com/studyolle/studyolle/study/StudyRepository.java
@@ -24,4 +24,6 @@ public interface StudyRepository extends JpaRepository<Study, Long> {
 
     @EntityGraph(attributePaths = "members")
     Study findStudyWithMembersByPath(String path);
+
+    Study findStudyOnlyByPath(String path);
 }
